9de54411818affcfa32a0e9335d2dd9c403674cd
Edidiong Archibong <eddymyk13@gmail.com>
2025-12-16 00:27:31 +0100

    update task_2.sql file

--- task_2.sql
+++ task_2.sql
@@ -10,7 +10,8 @@ CREATE TABLE Books (
     title VARCHAR(130),
     author_id INT,
     price DOUBLE,
-    publication_date DATE
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 CREATE TABLE Customers (
@@ -23,12 +24,16 @@ CREATE TABLE Customers (
 CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
-    order_date DATE
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+
 );
 
 CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity DOUBLE
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
